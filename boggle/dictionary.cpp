ced028c93d6a18abd391cdd91a1e3cad8f6ce08d
Alon Hershenhorn <hershen@gmail.com>
2022-01-24 14:12:27 -0800

    Changed dictionary to tree

--- boggle/dictionary.cpp
+++ boggle/dictionary.cpp
@@ -5,6 +5,35 @@
 #include <istream>
 #include <fstream>
 
+#include <iostream>
+const std::unordered_map<char, int> Dictionary::letter2index{
+	{'A', 0},
+	{ 'B', 1 },
+	{ 'C', 2 },
+	{ 'D', 3 },
+	{ 'E', 4 },
+	{ 'F', 5 },
+	{ 'G', 6 },
+	{ 'H', 7 },
+	{ 'I', 8 },
+	{ 'J', 9 },
+	{ 'K', 10 },
+	{ 'L', 11 },
+	{ 'M', 12 },
+	{ 'N', 13 },
+	{ 'O', 14 },
+	{ 'P', 15 },
+	{ 'Q', 16 },
+	{ 'R', 17 },
+	{ 'S', 18 },
+	{ 'T', 19 },
+	{ 'U', 20 },
+	{ 'V', 21 },
+	{ 'W', 22 },
+	{ 'X', 23 },
+	{ 'Y', 24 },
+	{ 'Z', 25 } };
+
 class FileNotFound : public std::exception {
 public:
 	FileNotFound(const std::string& message) : message_(message) {}
@@ -13,32 +42,64 @@ private:
 	std::string message_;
 };
 
-std::vector<std::string> readWords(std::istream& istream) {
+void Dictionary::addWord(const std::string& word) {
+	if (!dictionaryTree_) dictionaryTree_ = new DictionaryTree;
 
-	std::vector<std::string> words;
-	std::string word;
-	while (std::getline(istream, word)) {
-		words.push_back(word);
+	DictionaryTree* root = dictionaryTree_;
+	for (const auto& letter : word) {
+
+		if (!root->children[letter2index.at(letter)]) {
+			root->children[letter2index.at(letter)] = new DictionaryTree;
+			root->children[letter2index.at(letter)]->val = letter;
+		}
+
+		root = root->children[letter2index.at(letter)];
 	}
-	return words;
+	root->isWord = true;
 }
 
-std::vector<std::string> readWords(const std::string& filename) {
+void Dictionary::readWords(std::istream& istream) {
+
+	std::string word;
+	while (std::getline(istream, word)) {
+		addWord(word);
+	}
+}
+
+void Dictionary::readWords(const std::string& filename) {
 	std::ifstream ifstream(filename);
 	if (!ifstream.is_open()) {
 		throw FileNotFound("Could not find file: " + filename);
 	}
 
-	return readWords(ifstream);
+	readWords(ifstream);
 }
 
-Dictionary::Dictionary(const std::string& filename) : words_(readWords(filename)) {}
+Dictionary::Dictionary(const std::string& filename) { readWords(filename); }
 
-Dictionary::Dictionary(std::istream& istream) : words_(readWords(istream)) {}
+Dictionary::Dictionary(std::istream& istream) { readWords(istream); }
 
-bool Dictionary::isWord(const std::string& word) const {
-	return std::find(words_.begin(), words_.end(), word) != words_.end();
+//bool Dictionary::isWord(const std::string& word) const {
+//	return std::find(words_.begin(), words_.end(), word) != words_.end();
+//}
+
+void getWordsRecursive(const DictionaryTree* tree, std::string stub, std::vector<std::string>& words) {
+	if (tree->isWord) {
+		words.push_back(stub);
+	}
+	for (const auto& letter : tree->children) {
+		if (letter) {
+			getWordsRecursive(letter, stub + letter->val, words);
+		}
+	}
 }
+
+std::vector<std::string> Dictionary::getWords() const {
+	std::vector<std::string> words;
+	getWordsRecursive(dictionaryTree_, "", words);
+	return words;
+}
+
 
 
 
--- boggle/dictionary.hpp
+++ boggle/dictionary.hpp
@@ -1,16 +1,31 @@
 #pragma once
 
+#include <array>
 #include <string>
 #include <vector>
+#include <unordered_map>
+
+
+
+struct DictionaryTree {
+	std::array<DictionaryTree*, 26> children{nullptr};
+	char val = ' ';
+	bool isWord = false;
+};
 
 class Dictionary {
 public:
 	explicit Dictionary(const std::string& filename);
 	explicit Dictionary(std::istream& istream);
 
-	bool isWord(const std::string& word) const;
-	inline std::vector<std::string> getWords() const { return words_; }
+	//bool isWord(const std::string& word) const;
+	std::vector<std::string> getWords() const;
+
+	static const std::unordered_map<char, int> letter2index;
 
 private:
-	std::vector<std::string> words_;
+	DictionaryTree* dictionaryTree_;
+	void addWord(const std::string& word);
+	void readWords(std::istream& istream);
+	void readWords(const std::string& filename);
 };
